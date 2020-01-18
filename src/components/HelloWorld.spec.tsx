73ce6bbc0d2f999404d10d86dc27ae4d60d201cf
Germán Valencia <machinarius92@gmail.com>
2020-01-18 13:49:40 -0500

    Added a sample of a Jest snapshot test

--- src/components/HelloWorld.spec.tsx
+++ src/components/HelloWorld.spec.tsx
@@ -1,8 +1,18 @@
-import React = require('react');
+import * as React from 'react';
+import * as testRenderer from 'react-test-renderer';
+
 import { shallow } from 'enzyme';
 import HelloWorld from './HelloWorld'
 
-test("HelloWorld component", () => {
-    var component = shallow(<HelloWorld compiler="TypeScript" framework="Jest" />);
+test("Must show the hello message for the prop values passed in", () => {
+    let component = shallow(<HelloWorld compiler="TypeScript" framework="Jest" />);
     expect(component.text()).toBe("Hello from TypeScript and Jest");
+});
+
+test("Must match the snapshot", () => {
+    let component = testRenderer.create(
+        <HelloWorld compiler="TypeScript" framework="Jest" />
+    );
+    let treeData = component.toJSON();
+    expect(treeData).toMatchSnapshot();
 });
