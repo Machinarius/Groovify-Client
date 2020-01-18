94ea36f98c44030ce09bc4835bb0be5ff73ce31e
Germán Valencia <machinarius92@gmail.com>
2020-01-18 18:19:58 -0500

    Reconfigured Webpack to enable a clean Webpack-Dev-Server experience

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,10 @@ import * as ReactDOM from "react-dom";
 
 import HelloWorld from './components/HelloWorld';
 
+var targetElement = document.createElement("div");
+document.body.append(targetElement);
+
 ReactDOM.render(
     <HelloWorld compiler="TypeScript" framework="React" />,
-    document.getElementById('appRoot')
+    targetElement
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
-var DotEnvPlugin = require("dotenv-webpack");
+const path = require("path");
+
+const DotEnvPlugin = require("dotenv-webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackTagsPlugin = require('html-webpack-tags-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    mode: "production",
+    mode: "development",
     devtool: "source-map",
+    entry: path.resolve(__dirname, "src", "Index.tsx"),
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js"
+    },
     resolve: {
-        extensions: [".ts", ".tsx"]
+        extensions: [".wasm", ".mjs", ".js", ".jsx", ".ts", ".tsx", ".json"],
+        alias: {
+            "@": path.resolve("src"),
+        }
     },
     module: {
         rules: [{
@@ -24,6 +37,26 @@ module.exports = {
         "react-dom": "ReactDOM"
     },
     plugins: [
-        new DotEnvPlugin()
-    ]
+        new DotEnvPlugin(),
+        new CopyPlugin([{ 
+            from: "./node_modules/react/umd/react.development.js",
+            to: "./vendor/react.development.js"
+        }, {
+            from: "./node_modules/react-dom/umd/react-dom.development.js",
+            to: "./vendor/react-dom.development.js"
+        }]),
+        new HtmlWebpackPlugin(),
+        new HtmlWebpackTagsPlugin({
+            tags: [ "./vendor/react.development.js", "./vendor/react-dom.development.js" ],
+            append: false
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+        allowedHosts: [
+            ".localtest.me"
+        ]
+    }
 }
