8a7f639bcc626ba19073013c5cc1c34b4fd2eff0
Germán Valencia <machinarius92@gmail.com>
2020-02-23 22:01:01 -0500

    Removed a component that no longer exists from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import HelloWorld from './components/HelloWorld';
 import AppContainer from "./components/AppContainer";
 
 var targetElement = document.createElement("div");
