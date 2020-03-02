d565dd736ce1d7e994161ee453046480f583f6bc
Germán Valencia <machinarius92@gmail.com>
2020-03-02 08:17:55 -0500

    Mocked CSS imports on jest executions

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFilesAfterEnv: ["<rootDir>/setup_tests.ts"]
+  setupFilesAfterEnv: ["<rootDir>/setup_tests.ts"],
+  moduleNameMapper: {
+    "\\.(css|less)$": "<rootDir>/__mocks__/style_mock.js"
+  },
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "/dist/"
+  ]
 };
